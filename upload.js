df573c3768caabe1fa9c62f94e824c382ba7cf7f
samelie <sam@stinkdigital.com>
2017-02-21 00:31:05 -0800

    upload fix?

--- upload.js
+++ upload.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 
 
 const spawn = require('child_process').spawn
-const exec = require('child_process').execSync
+const exec = require('child_process').exec
 
 const UPLOAD = (() => {
     const PATH = process.env.YOUTUBE_UPLOAD || 'youtube-upload'
@@ -13,20 +13,12 @@ const UPLOAD = (() => {
     function upload(path, options) {
         return new Q((yes, no) => {
             const _cmd = `${PATH} --title ${options.title} ${path}`
-            console.log(_cmd);
             const child = exec(_cmd)
-                // Listen for stdout data
-            child.stderr.on('data', function(data) {
-                console.log(data.toString());
-            })
+
             child.stdout.on('data', function(data) {
-                console.log("Done");
-                fs.unlinkSync(path)
-                yes(data)
-                console.log("Got data from child: " + data);
+                yes(data.toString())
             });
-            // Listen for an exit event:
-            child.on('exit', function(exitCode) {
+            child.on('exit', function(exitCode, err, data) {
                 if (exitCode !== 0) {
                     //err
                     console.log("Error");
@@ -34,7 +26,6 @@ const UPLOAD = (() => {
                 } else {
 
                 }
-                console.log("Child exited with code: " + exitCode);
             });
         })
     }
@@ -46,5 +37,6 @@ const UPLOAD = (() => {
 
 })()
 
+//UPLOAD.upload("2c200ee2-bbe1-49dd-931e-eb09c9679287.mp4", {})
 
 module.exports = UPLOAD
